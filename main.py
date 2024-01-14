12148ccbb4da72bae823025bf626b39b9ec6c9e5
VladVizonok <vlad.vizonok@gmail.com>
2024-01-14 12:47:33 +0200

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sys import exit
 
 main_user_dict = {}
 
+# Створюємо декоратор для виключень
 def input_error(func):
     def inner(*args, **kwargs):
         while True:
@@ -15,13 +16,16 @@ def input_error(func):
                 print ("Invalid input. Please try again.")
     return inner
 
-
+# Функція для привітання 
 def hello():
     print('How I can help you?')
 
-
+"""
+Функція для додавання номеру перевіряє чи імʼя користувача складається з одного слова чи з двох 
+та чи був введений номер телефону.
+"""
 def add(input):
-    if input[2].isnumeric():
+    if input[2].isnumeric(): 
         name = input[1].title()
         main_user_dict[name] = input[2]
     elif input[3].isnumeric():
@@ -31,7 +35,7 @@ def add(input):
         raise ValueError
     print('Contact added.')
 
-
+#Функція зміни номеру телефону діє ідентично до додавання, просто замінюючи значення в словнику.
 def change(input):
     if input[2].isnumeric():
         name = input[1].title()
@@ -43,7 +47,7 @@ def change(input):
         raise ValueError
     print('Contact changed.')
 
-
+#Функція виводу телефону також розрізняє контакти з одним словом в імені і з двома.
 def phone(input):
     if len(input) > 2:
         name = input[1].title() + ' ' + input[2].title()
@@ -51,15 +55,16 @@ def phone(input):
         name = input[1].title()
     print(f'Phone is: {main_user_dict[name]}')
 
-
+#Функція виводу словника з всіма контактами.
 def show_all():
     print(f'All your contacts: {main_user_dict}')
 
-
+#Фукція прощання.
 def good_bye():
     print('Good bye!')
     exit()
 
+#Декоратор обробляє виключенняБ що виникають в main.
 @input_error
 def main(): 
     while True:
@@ -81,7 +86,7 @@ def main():
         elif command == 'phone': 
             phone(user_input)
 
-        elif len(user_input) > 1:
+        elif len(user_input) > 1:  #Перевірка на довжину, щоб сприймати команди з двох слів.
             if command + ' ' + user_input[1] == 'show all':
                 show_all()
             if command + ' ' + user_input[1] == 'good bye':
